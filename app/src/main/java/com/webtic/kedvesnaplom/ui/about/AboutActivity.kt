a376f1aa7f667673bcdf14866898f183dc1b25c5
Mark Toth <tothmarkandor99@freemail.hu>
2022-05-16 02:27:16 +0200

    Dependencies updated

--- app/src/main/java/com/webtic/kedvesnaplom/ui/about/AboutActivity.kt
+++ app/src/main/java/com/webtic/kedvesnaplom/ui/about/AboutActivity.kt
@@ -3,7 +3,7 @@ package com.webtic.kedvesnaplom.ui.about
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,7 +14,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.webtic.kedvesnaplom.R
 
 class AboutActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,11 +36,8 @@ fun AboutPage() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "First item",
-            color = MaterialTheme.colors.onPrimary,
-            modifier = Modifier.padding(16.dp)
-        )
+        val image: Painter = painterResource(id = R.drawable.ic_launcher_background)
+        Image(painter = image,contentDescription = "")
         Text(
             text = "Second item",
             color = MaterialTheme.colors.onPrimary,
